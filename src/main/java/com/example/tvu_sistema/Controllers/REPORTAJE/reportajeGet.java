2f4bdd35bac782ded8d269a162edde7463900ec7
marcocuellar7293@gmail.com <marcocuellar7293@gmail.com>
2024-04-26 11:57:50 -0400

    asdasd

--- src/main/java/com/example/tvu_sistema/Controllers/REPORTAJE/reportajeGet.java
+++ src/main/java/com/example/tvu_sistema/Controllers/REPORTAJE/reportajeGet.java
@@ -1,4 +1,5 @@
 package com.example.tvu_sistema.Controllers.REPORTAJE;
+
 import java.sql.Time;
 import java.sql.Timestamp;
 import java.text.DateFormat;
@@ -16,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,68 +44,69 @@ import com.example.tvu_sistema.Models.Repository.ProgramaR.ProgramaRepository;
 public class reportajeGet {
 
     @Autowired
-	private IProgramaService programaService;
+    private IProgramaService programaService;
 
     @Autowired
-	private IPersonaService personaService;
+    private IPersonaService personaService;
 
     @Autowired
-	private ICategoria_reportajeService categoria_reportajeService;
+    private ICategoria_reportajeService categoria_reportajeService;
 
     @Autowired
-	private IReportajeService reportajeService;
+    private IReportajeService reportajeService;
 
     @Autowired
-	private ProgramaRepository programaRepository;
-    
+    private ProgramaRepository programaRepository;
+
     @RequestMapping(value = "RegistroReportajeA", method = RequestMethod.GET) // Pagina principal
-    public String RegistroPersonaV( @Validated Reportaje reportaje, Model model,RedirectAttributes flash,HttpServletRequest request) {
+    public String RegistroPersonaV(@Validated Reportaje reportaje, Model model, RedirectAttributes flash,
+            HttpServletRequest request) {
 
         if (request.getSession().getAttribute("persona") != null) {
 
             List<Reportaje> reportajes = reportajeService.findAll();
-          
+
             model.addAttribute("reportajes", reportajes);
-     
 
             model.addAttribute("programas", programaService.findAll());
             model.addAttribute("personas", personaService.findAll());
             model.addAttribute("categoria_reportajes", categoria_reportajeService.findAll());
             model.addAttribute("ano_actual", programaRepository.anoActual());
-                        
+
             return "reportaje/registroReportajeA";
-        }else{
+        } else {
             return "redirect:/LoginV";
         }
     }
 
-    @GetMapping("/tableReportaje")
-    public String tableReportaje(@Validated Reportaje reportaje, Model model,RedirectAttributes flash,HttpServletRequest request) throws Exception {
+    @PostMapping("/tableReportaje")
+    public String tableReportaje(@Validated Reportaje reportaje, Model model, RedirectAttributes flash,
+            HttpServletRequest request) throws Exception {
 
-        List<Reportaje> reportajes = reportajeService.lista_reportajes();
-        List<String> encryptedIds = new ArrayList<>();
-        for (Reportaje reportaje2 : reportajes) {
-            try {
-                String id_encryptado = Encryptar.encrypt(Long.toString(reportaje2.getId_reportaje()));
-                encryptedIds.add(id_encryptado);    
-            } catch (Exception e) {
-                // TODO: handle exception
-                System.out.println(e);
-            }
-            
-        }
+        List<Reportaje> reportajes = reportajeService.findAll();
+
         model.addAttribute("reportajes", reportajes);
-        model.addAttribute("id_encryptado", encryptedIds);
 
         model.addAttribute("programas", programaService.findAll());
         model.addAttribute("personas", personaService.findAll());
         model.addAttribute("categoria_reportajes", categoria_reportajeService.findAll());
+
+        return "reportaje/tableReportaje";
+    }
+
+    @PostMapping(value = "/NuevoReportaje")
+    public String NuevoReportaje(HttpServletRequest request, Model model) {
+        model.addAttribute("reportaje", new Reportaje());
+        model.addAttribute("programas", programaService.findAll());
+        model.addAttribute("personas", personaService.findAll());
+        model.addAttribute("categoria_reportajes", categoria_reportajeService.findAll());
+    
         
-        return "reportaje/tableReportaje :: table";
+        return "reportaje/formReportaje";
     }
 
     @RequestMapping(value = "/eliminar-reportaje/{id_reportaje}")
-       public String eliminar_r(@PathVariable("id_reportaje") String id_reportaje) throws Exception {
+    public String eliminar_r(@PathVariable("id_reportaje") String id_reportaje) throws Exception {
         try {
             Long id_repor = Long.parseLong(Encryptar.decrypt(id_reportaje));
             Reportaje reportaje = reportajeService.findOne(id_repor);
@@ -117,35 +120,18 @@ public class reportajeGet {
     }
 
     @RequestMapping(value = "/editar-reportaje/{id_reportaje}")
-    public String editar_r(@PathVariable("id_reportaje") String id_reportaje, Model model) {
+    public String editar_r(@PathVariable("id_reportaje")  Long id_reportaje, Model model) {
         try {
-            Long id_repor = Long.parseLong(Encryptar.decrypt(id_reportaje));
-            Reportaje reportaje = reportajeService.findOne(id_repor);
+          
+            Reportaje reportaje = reportajeService.findOne(id_reportaje);
             model.addAttribute("reportaje", reportaje);
-
-            //model.addAttribute("hr_empiezo_pogramaa", programa.getHr_empiezo_pograma());
-
-            List<Reportaje> reportajes = reportajeService.lista_reportajes();
-            List<String> encryptedIds = new ArrayList<>();
-            for (Reportaje reportaje2 : reportajes) {
-                try {
-                    String id_encryptado = Encryptar.encrypt(Long.toString(reportaje2.getId_reportaje()));
-                    encryptedIds.add(id_encryptado);    
-                } catch (Exception e) {
-                    // TODO: handle exception
-                    System.out.println(e);
-                }
-                
-            }
-            model.addAttribute("reportajes", reportajes);
-            model.addAttribute("id_encryptado", encryptedIds);
 
             model.addAttribute("programas", programaService.findAll());
             model.addAttribute("personas", personaService.findAll());
             model.addAttribute("categoria_reportajes", categoria_reportajeService.findAll());
             model.addAttribute("ano_actual", programaRepository.anoActual());
 
-            return "reportaje/registroReportajeA";
+            return "reportaje/formReportaje";
 
         } catch (Exception e) {
 
@@ -155,13 +141,11 @@ public class reportajeGet {
 
     @RequestMapping(value = "/RegistroReportaje2F", method = RequestMethod.POST) // Enviar datos de Registro a Lista
     public String RegistroReportaje2F(@Validated Reportaje reportaje, RedirectAttributes redirectAttrs,
-    @RequestParam(name="f_reportajee",required = false)String f_reportajee
-    ) throws ParseException {
+            @RequestParam(name = "f_reportajee", required = false) String f_reportajee) throws ParseException {
 
         DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         Date f_reportaje = formatter.parse(f_reportajee);
-        
-        
+
         reportaje.setF_reportaje(f_reportaje);
         reportaje.setEst_reportaje("A");
 
@@ -170,16 +154,17 @@ public class reportajeGet {
     }
 
     @GetMapping("/modalReportaje/{id_reportaje}")
-    public String modalReportaje(@PathVariable("id_reportaje") Long id_reportaje,Model model) {
-        
+    public String modalReportaje(@PathVariable("id_reportaje") Long id_reportaje, Model model) {
+
         Reportaje reportaje = reportajeService.findOne(id_reportaje);
         model.addAttribute("reportaje", reportaje);
 
         return "reportaje/modalReportaje :: modalContent";
     }
+
     @GetMapping("/modalReportaje2/{id_reportaje}")
-    public String modalReportaje2(@PathVariable("id_reportaje") Long id_reportaje,Model model) {
-        
+    public String modalReportaje2(@PathVariable("id_reportaje") Long id_reportaje, Model model) {
+
         Reportaje reportaje = reportajeService.findOne(id_reportaje);
         model.addAttribute("reportaje", reportaje);
 
--- src/main/java/com/example/tvu_sistema/Controllers/REPORTAJE/reportajePost.java
+++ src/main/java/com/example/tvu_sistema/Controllers/REPORTAJE/reportajePost.java
@@ -4,22 +4,22 @@ import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.tvu_sistema.Models.Entity.Dias_transmision;
-import com.example.tvu_sistema.Models.Entity.Programa;
 import com.example.tvu_sistema.Models.Entity.Reportaje;
-import com.example.tvu_sistema.Models.Entity.Transmite;
+import com.example.tvu_sistema.Models.Entity.Usuario;
 import com.example.tvu_sistema.Models.IService.IProgramaService;
 import com.example.tvu_sistema.Models.IService.IReportajeService;
 
@@ -32,33 +32,35 @@ public class reportajePost {
     @Autowired
 	private IReportajeService reportajeService;
     
+     @PostMapping(value = "admin/RegistroReportajeF")
+    public ResponseEntity<String> RegistroReportajeF(@Validated Reportaje reportaje, RedirectAttributes flash,
+            HttpServletRequest request,
+            @RequestParam(name="f_reportajee",required = false)String f_reportajee
+           ) throws ParseException {
 
-    @PostMapping(value = "RegistroReportajeF")
-    public String RegistroReportajeF(@Validated Reportaje reportaje,RedirectAttributes flash,HttpServletRequest request,
-    @RequestParam(name="f_reportajee",required = false)String f_reportajee
-    ) throws ParseException{          
-        
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        Date f_reportaje = formatter.parse(f_reportajee);
-        
-        Timestamp timestamp = new Timestamp(f_reportaje.getTime());
+            DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date f_reportaje = formatter.parse(f_reportajee);
+            
+            Timestamp timestamp = new Timestamp(f_reportaje.getTime());
 
-        Long respuesta = reportajeService.insertar_reportaje(
-            reportaje.getTema_reportaje(), 
-            reportaje.getDesc_reportaje(),
-            reportaje.getLugar_reportaje(), 
-            timestamp, 
-            reportaje.getInvitados_reportaje(),
-            reportaje.getUrl_video_reportaje(),
-            Math. toIntExact(reportaje.getPrograma().getId_programa()),
-            Math. toIntExact(reportaje.getPersona().getId_persona()),
-            Math. toIntExact(reportaje.getCategoria_reportaje().getId_categoria_reportaje())
-            );
-
-        if (respuesta != 0) {
-            return "1";
-        }else{
-            return "0";
-        }        
+        Usuario usuario = (Usuario) request.getSession().getAttribute("usuario");
+            
+        if (usuario != null) {
+            if (reportaje.getId_reportaje() == null) {
+                reportaje.setEst_reportaje("A");
+                reportaje.setF_reportaje(f_reportaje);
+                reportajeService.save(reportaje);
+                return ResponseEntity.ok("Se realizó el registro correctamente");
+            }else{
+                reportaje.setEst_reportaje("A");
+                reportaje.setF_reportaje(f_reportaje);
+                reportajeService.save(reportaje);
+                return ResponseEntity.ok("Se edito el registro correctamente");
+            }
+               
+        } else {
+            return ResponseEntity.ok("Error Al Registrar Reportaje");
+        }
     }
+   
 }
